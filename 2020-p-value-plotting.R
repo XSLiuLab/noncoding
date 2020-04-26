762ed97ae0a5e5bf20cfd9d8e94167206825809f
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-04-26 21:28:06 +0800

    update

--- 2020-p-value-plotting.R
+++ 2020-p-value-plotting.R
@@ -6,17 +6,20 @@ load(file = "RegionMutationList.RData")
 
 prob_region_final[, p_value := -log10(p_val)][, p_value := ifelse(p_value > 15, 15, p_value)]
 
-#pos <- position_jitter(width = 0.2, height = 0.03, seed = 2)
+pos <- position_jitter(width = 0.2, height = 0.03, seed = 2)
 p1 = ggplot(prob_region_final[gene_name != "BCL2"], aes(x = count, y = p_value)) + 
-  geom_point(color = ifelse(prob_region_final[gene_name != "BCL2"]$count > 20, "red", "black")) +
-  geom_label_repel(data = dplyr::filter(prob_region_final, count > 20 & gene_name != "BCL2"),
+  geom_jitter(color = ifelse(prob_region_final[gene_name != "BCL2"]$count > 20, "red", "black"),
+              position = pos, size = 1, alpha = 0.7) +
+  geom_text_repel(data = dplyr::filter(prob_region_final, count > 20 & gene_name != "BCL2"),
                    aes(label = gene_name),
-                   size = 2,
-                   nudge_y = -1,
-                   segment.size = 0.2) +
+                  hjust = 1,
+                  position = pos,
+                  segment.size = 0.4,min.segment.length = 0.1,
+                  size = 2) +
   labs(x = "Number of samples with base mutated", y = "-log10 P value") +
   cowplot::theme_cowplot(rel_small = 1.5) 
 
+p1
 
 ggsave(filename = "pvalue_vs_count.pdf", plot = p1, width = 7, height = 5)
 
